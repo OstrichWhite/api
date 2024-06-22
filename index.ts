f5c3beb67887e4cb802dafc28893d8e8970d5971
OstrichWhite <rahimcsestudies@gmail.com>
2024-06-23 01:06:20 +0530

    Introduced stupServer.

--- index.ts
+++ index.ts
@@ -1,19 +1,12 @@
 import { config } from "dotenv";
 config()
 
-import express from "express";
 import connectDB from "./connectDB";
-
-const app = express();
-const {PORT: port = 3000 }= process.env;
+import setupServer from "./setupServer";
 
 const main = async () =>{
-  app.get("/info", (req, res) => {
-    res.json({ message: "hello from server." });
-  });
-
   await connectDB()
-  app.listen(port, () => console.log(`Server listening on port ${port}.`));  
+  setupServer()
 }
 
 main()
