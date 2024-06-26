f58a32f19315ec9a7c1cce1f860265ee1645b69d
OstrichWhite <rahimcsestudies@gmail.com>
2024-06-26 07:58:17 +0530

    Implemented buildCRUD in products router.

--- routes/products.ts
+++ routes/products.ts
@@ -1,9 +1,12 @@
 import { Router, Response } from "express";
+import buildCRUD from "../utils/buildCRUD";
+import products, { Products } from "../models/products";
 
-const router = Router();
-
-router.get('/', (req, res: Response) => {
+const router = Router()
+router.get('/hi', (req, res: Response) => {
   res.json({message: "hi"})
 })
 
-export default router
+const productsRouter: Router = buildCRUD<Products>(products);
+
+export default productsRouter;
